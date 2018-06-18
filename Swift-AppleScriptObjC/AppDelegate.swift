a3cb30ac3edca085aa4a823328cbe5a1211a16e6
hhas <hhas@users.sourceforge.net>
2018-06-18 18:45:23 +0100

    Initial Commit

---
+++ Swift-AppleScriptObjC/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swift-AppleScriptObjC
+//
+//  Created by Hamish Sanderson on 18/06/2018.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
